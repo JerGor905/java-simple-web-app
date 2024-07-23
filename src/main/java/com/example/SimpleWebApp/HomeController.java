a13fd77e5e2aae135d0526b69656188df5f44fd8
Jeffery Tse <kytseag@ust.hk>
2024-07-24 01:03:31 +0800

    feat: implement Home page with RestController

---
+++ src/main/java/com/example/SimpleWebApp/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.SimpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet() {
+        return "Welcome to the Home Page!";
+    }
+
+}
