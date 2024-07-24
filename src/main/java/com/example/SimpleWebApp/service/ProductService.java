36cc7cc092b669f2c60381b2f9854429b9df4e1c
Jeffery Tse <kytseag@ust.hk>
2024-07-24 13:09:01 +0800

    feat: implement GET and POST methods for Product class

--- src/main/java/com/example/SimpleWebApp/service/ProductService.java
+++ src/main/java/com/example/SimpleWebApp/service/ProductService.java
@@ -3,20 +3,34 @@ package com.example.SimpleWebApp.service;
 import com.example.SimpleWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
-            new Product(1, "AE86", 238000),
-            new Product(2, "FD3S", 268000),
-            new Product(3, "BNR32", 448000)
+    List<Product> products = new ArrayList<>(
+            Arrays.asList(
+                    new Product(1, "AE86", 238000),
+                    new Product(2, "FD3S", 268000),
+                    new Product(3, "BNR32", 448000)
+            )
     );
 
     public List<Product> getProducts() {
         return products;
+    }
+
+    public Product getProductById(int id) {
+        return products.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst()
+                .orElse(new Product(0, "No such item", 0));
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
     }
 
 }
